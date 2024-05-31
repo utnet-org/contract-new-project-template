33b0b9c3e9d30bc188716514e0c6c8ebfcac094c
jong-un <36259838+zwong91@users.noreply.github.com>
2024-05-31 16:27:31 +0800

    chore: Add sleep to deploy_all.sh for confirming contract upload

--- deploy_all.sh
+++ deploy_all.sh
@@ -7,6 +7,9 @@ pushd $(dirname ${BASH_SOURCE[0]})
 echo ">> Deploying contract"
 for wasm_file in $(find res -name "*.wasm"); do
     unc contract deploy $ACCOUNT use-file "$wasm_file" without-init-call network-config testnet sign-with-keychain send
+    # Sleep a bit to let the previous contract upload to blockchain. Otherwise we fail publishing checks.
+    echo "sleeping for wait for 6 blocks to confirm..."
+    sleep 120
 done
 
 popd
