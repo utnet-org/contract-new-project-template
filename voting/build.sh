2f9384de189909e9d803ab4f53ecd2dd30e18669
zwong91 <yzbingchuan@gmail.com>
2024-06-02 15:05:45 +0800

    chore: Update build.sh to copy wasm file to correct directory

--- voting/build.sh
+++ voting/build.sh
@@ -3,4 +3,4 @@ TARGET="${CARGO_TARGET_DIR:-../target}"
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp $TARGET/wasm32-unknown-unknown/release/voting_contract.wasm ./res/
+cp $TARGET/wasm32-unknown-unknown/release/voting_contract.wasm ../res/
