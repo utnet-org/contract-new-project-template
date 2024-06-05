768ed7b088376f3f13c3ed5d706e7bb9abb47b77
jong-un <36259838+zwong91@users.noreply.github.com>
2024-06-05 15:40:35 +0800

    chore: Update deploy.sh script to deploy contract with additional parameters

--- social-web3/deploy.sh
+++ social-web3/deploy.sh
@@ -3,5 +3,11 @@ ACCOUNT="${CONTRACT_ACCOUNT_ID:-unc}"
 set -e
 
 ./build.sh
-unc dev-tool deploy $ACCOUNT
 
+cd "$(dirname $0)"
+pushd contract
+
+#unc dev-tool deploy $ACCOUNT
+unc contract deploy unc use-file ../../res/social_web3.wasm without-init-call network-config testnet sign-with-keychain send
+
+popd
