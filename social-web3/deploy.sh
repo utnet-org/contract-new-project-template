d39dfa733fbbec62b4f903aac42bfca5f32fde8c
jong-un <36259838+zwong91@users.noreply.github.com>
2024-06-05 19:14:12 +0800

    chore: Update social-web3/deploy.sh script
    The commit updates the social-web3/deploy.sh script. The changes include modifying the unc contract deploy command to use the ${ACCOUNT} variable and adding additional arguments for the contract deployment. These changes improve the deployment process by ensuring the correct account is used and providing necessary configuration options.

--- social-web3/deploy.sh
+++ social-web3/deploy.sh
@@ -8,6 +8,5 @@ cd "$(dirname $0)"
 pushd contract
 
 #unc dev-tool deploy $ACCOUNT
-unc contract deploy unc use-file ../../res/social_web3.wasm without-init-call network-config testnet sign-with-keychain send
-
+unc contract deploy ${ACCOUNT} use-file ../../res/social_web3.wasm with-init-call new json-args {} prepaid-gas '100.0 Tgas' attached-deposit '0 unc' network-config testnet sign-with-keychain send
 popd
