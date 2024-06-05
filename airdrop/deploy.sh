2b8825419d935f8d163e8d13bde3178701c88a40
jong-un <36259838+zwong91@users.noreply.github.com>
2024-06-05 15:36:36 +0800

    chore: Update deploy.sh scripts with additional deployment parameters

--- airdrop/deploy.sh
+++ airdrop/deploy.sh
@@ -3,5 +3,5 @@ ACCOUNT="${CONTRACT_ACCOUNT_ID:-unc}"
 set -e
 
 ./build.sh
-unc dev-tool deploy $ACCOUNT
+unc dev-tool deploy $ACCOUNT with-init-call new json-args {} prepaid-gas '100.0 Tgas' attached-deposit '0 unc' network-config testnet sign-with-keychain send
 
--- hello-utility/deploy.sh
+++ hello-utility/deploy.sh
@@ -4,4 +4,4 @@ ACCOUNT="${CONTRACT_ACCOUNT_ID:-unc}"
 
 echo ">> Deploying contract"
 #unc contract deploy unc use-file ./target/wasm32-unknown-unknown/release/contract.wasm without-init-call network-config testnet sign-with-keychain send
-unc dev-tool deploy $ACCOUNT
+unc dev-tool deploy $ACCOUNT with-init-call new json-args '{"beneficiary": "unc"}' prepaid-gas '100.0 Tgas' attached-deposit '0 unc' network-config testnet sign-with-keychain send
